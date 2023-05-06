d3e369aee1466da1b04454c65b966ca81b1d7807
K1NGAARON <gerardaaron1997@gmail.com>
2023-05-06 10:49:25 +0200

    changes

--- js/cards.js
+++ js/cards.js
@@ -1,6 +1,6 @@
 const content = [
     {
-        title: "Apple Music Live returns for a brand-new season with Ed Sheeran",
+        title: "1. Apple Music Live returns for a brand-new season with Ed Sheeran",
         tag: "health",
         category: "podcast",
         img: "/posts/img/image1.jpg",
@@ -8,7 +8,7 @@ const content = [
         date: "2023-05-05",
     },
     {
-        title: "Apple reports second quarter results",
+        title: "2. Apple reports second quarter results",
         tag: "health",
         category: "podcast",
         img: "/posts/img/image2.jpg",
@@ -16,7 +16,7 @@ const content = [
         date: "2023-04-05",
     },
     {
-        title: "Apple launches 20 fun new games for its award-winning Apple Arcade service",
+        title: "3. Apple launches 20 fun new games for its award-winning Apple Arcade service",
         tag: "business",
         category: "blog",
         img: "/posts/img/image3.jpg",
@@ -24,7 +24,7 @@ const content = [
         date: "2023-03-27",
     },
     {
-        title: "Apple, Google partner on an industry specification to address unwanted tracking",
+        title: "4. Apple, Google partner on an industry specification to address unwanted tracking",
         tag: "Health",
         category: "blog",
         img: "/posts/img/image4.jpg",
@@ -32,7 +32,7 @@ const content = [
         date: "2022-08-14",
     },
     {
-        title: "Apple Saket will open this Thursday, April 20, in New Delhi",
+        title: "5. Apple Saket will open this Thursday, April 20, in New Delhi",
         tag: "science",
         category: "podcast",
         img: "/posts/img/image5.jpg",
@@ -40,26 +40,75 @@ const content = [
         date: "2022-01-03",
     },
     {
-        title: "Apple Card's new high-yield Savings account is now available, offering a 4.15 percent APY",
+        title: "6. Apple Card's new high-yield Savings account is now available, offering a 4.15 percent APY",
         tag: "science",
         category: "blog",
         img: "/posts/img/image6.jpg",
         link: "",
         date: "2023-05-02",
     },
+    {
+        title: "7. Apple Music Live returns for a brand-new season with Ed Sheeran",
+        tag: "health",
+        category: "podcast",
+        img: "/posts/img/image1.jpg",
+        link: "post-1/",
+        date: "2023-05-05",
+    },
+    {
+        title: "8. Apple Saket will open this Thursday, April 20, in New Delhi",
+        tag: "science",
+        category: "podcast",
+        img: "/posts/img/image5.jpg",
+        link: "",
+        date: "2022-01-03",
+    },
+    {
+        title: "9. Apple launches 20 fun new games for its award-winning Apple Arcade service",
+        tag: "business",
+        category: "blog",
+        img: "/posts/img/image3.jpg",
+        link: "",
+        date: "2023-03-27",
+    },
+    {
+        title: "10. Apple reports second quarter results",
+        tag: "health",
+        category: "podcast",
+        img: "/posts/img/image2.jpg",
+        link: "",
+        date: "2023-04-05",
+    },
+    {
+        title: "11. Apple, Google partner on an industry specification to address unwanted tracking",
+        tag: "Health",
+        category: "blog",
+        img: "/posts/img/image4.jpg",
+        link: "",
+        date: "2022-08-14",
+    },
+    {
+        title: "12. Apple Saket will open this Thursday, April 20, in New Delhi",
+        tag: "science",
+        category: "podcast",
+        img: "/posts/img/image5.jpg",
+        link: "",
+        date: "2022-01-03",
+    },
 ];
 
 // Future reference
 // posts.sort((a, b) => new Date(b.date) - new Date(a.date));
+// => create a sorting function
 
+const highlightedToShow = 6
+const archivedToShow = 8
 
-function createCards(e) {
+function highlightedContent(e) {
     const target = document.querySelector('#content');
 
-    const cards = content.map(content => {
+    const cards = content.slice(0, highlightedToShow).map(content => {
         // const tagWithSpaces = content.tag.replace(/-/g, ' ');
-
-        // Check the href on the a tag wrapping this card.
 
         return `
             <div class="card ${content.date} ${content.category} ${content.tag}">
@@ -83,4 +132,40 @@ function createCards(e) {
     target.innerHTML = cards;
 };
 
-$(document).ready(createCards);
+
+function archiveContent(e) {
+    const target = document.querySelector('#archive');
+
+    const cards = content.slice(highlightedToShow, highlightedToShow + archivedToShow).map(content => {
+        // const tagWithSpaces = content.tag.replace(/-/g, ' ');
+
+        return `
+            <div class="card-wrapper">
+                <img src="${content.img}" alt="${content.title}">
+                <div class="card archive-card ${content.date} ${content.category} ${content.tag}">
+                    <div class="card-content">
+                        <p class="tag">
+                            ${content.tag}
+                        </p>
+                        <h5>
+                            ${content.title}
+                        </h5>
+                        <a href="/posts/${content.link}" class="btn ghost">
+                            Lees meer
+                        </a>
+                    </div>
+                </div>
+            </div>
+        `;
+
+
+    }).join('');
+
+    target.innerHTML = cards;
+}
+
+
+$(document).ready(function() {
+    highlightedContent();
+    archiveContent();
+});

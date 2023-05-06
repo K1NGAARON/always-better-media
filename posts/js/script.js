01f9f1317ffaa4c253e87066171162368ab331ef
K1NGAARON <gerardaaron1997@gmail.com>
2023-05-06 13:58:23 +0200

    changes

--- posts/js/script.js
+++ posts/js/script.js
@@ -27,4 +27,37 @@ function createAllContent(e) {
     }
 };
 
-$(document).ready(createAllContent);
+function highlightedContent(e) {
+    const target = document.querySelector('#relatedContent');
+    
+    if (target) {
+        const cards = content.slice(0, 4).map(content => {
+
+            return `
+                <div class="card ${content.date} ${content.category} ${content.tag}">
+                    <img src="${content.img}" alt="${content.title}">
+                    <div class="card-content">
+                        <p class="tag">
+                            ${content.tag}
+                        </p>
+                        <h5>
+                            ${content.title}
+                        </h5>
+                        <a href="/posts/${content.link}" class="btn ghost">
+                            Lees meer
+                        </a>
+                    </div>
+                    <div class="overlay"></div>
+                </div>
+            `;
+        }).join('');
+    
+        target.innerHTML = cards;
+    }
+};
+
+
+$(document).ready(function() {
+    createAllContent();
+    highlightedContent()
+});
